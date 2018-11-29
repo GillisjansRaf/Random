0e2320db1cc130b17ff966ccaddf3d880cbd8328
GillisjansRaf <raf.gillisjans@student.howest.be>
2018-11-29 10:37:45 +0100

    BackEnd

---
+++ src/main/java/GoldenEagle.java
@@ -0,0 +1,78 @@
+public class GoldenEagle {
+
+    public GoldenEagle(){
+
+    }
+    public static void main(String[] args) {
+        convert(3998);
+    }
+    public static String convert(int input){
+        String romanValue="";
+        int toConvert = input;
+        if(toConvert > 0 || toConvert < 3999){
+            while(toConvert>0){
+                while (toConvert >= 1000){
+                    romanValue+="M";
+                    toConvert-=1000;
+                    System.out.println(toConvert+" "+romanValue);
+                }
+                while(toConvert >=900){
+                    romanValue+="CM";
+                    toConvert-=900;
+                    System.out.println(toConvert+" "+romanValue);
+                }
+                while(toConvert >= 500){
+                    romanValue+="D";
+                    toConvert-=500;
+                    System.out.println(toConvert+" "+romanValue);
+                }
+                while(toConvert >= 400){
+                    romanValue+="CD";
+                    toConvert-=400;
+                    System.out.println(toConvert+" "+romanValue);
+                }
+                while(toConvert >= 100){
+                    romanValue+="C";
+                    toConvert-=100;
+                    System.out.println(toConvert+" "+romanValue);
+                }
+                while (toConvert >= 50){
+                    romanValue+="L";
+                    toConvert-=50;
+                    System.out.println(toConvert+" "+romanValue);
+                }
+                while(toConvert >= 40){
+                    romanValue+="XL";
+                    toConvert-=40;
+                    System.out.println(toConvert+" "+romanValue);
+                }
+                while(toConvert >= 10){
+                    romanValue+="X";
+                    toConvert -= 10;
+                    System.out.println(toConvert+" "+romanValue);
+                }
+                while (toConvert >= 9){
+                    romanValue+="IX";
+                    toConvert-=9;
+                    System.out.println(toConvert+" "+romanValue);
+                }
+                while(toConvert>=5){
+                    romanValue+="V";
+                    toConvert-=5;
+                    System.out.println(toConvert+" "+romanValue);
+                }
+                while (toConvert>=4){
+                    romanValue+="IV";
+                    toConvert-=4;
+                    System.out.println(toConvert+" "+romanValue);
+                }
+                while (toConvert>0){
+                    romanValue+="I";
+                    toConvert-=1;
+                    System.out.println(toConvert+" "+romanValue);
+                }
+            }
+        }
+        return romanValue;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rafgi
+  Date: 29/11/2018
+  Time: 09:52
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <button onclick=""
+  </body>
+</html>
